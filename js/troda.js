e25bbeef0956d60b5e90e7638fa387a0d84a61d3
Sjur Kvammen <sjur.kvammen@gmail.com>
2014-02-07 21:47:12 +0100

    with troda lib

--- js/troda.js
+++ js/troda.js
@@ -1,5 +1,11 @@
-        function troda_getURLParameter(name) {
-            return decodeURI(
-                    (RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[,null])[1]
-            );
-        }
+function troda_getURLParameter(name) {
+    return decodeURI(
+        (RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[,null])[1]
+    );
+}
+
+var troda = function(){
+    return{
+        id:5
+    }
+}();
