61f25ddd0230f570482b916039553b86db58000e
Espen <espen@dbwatch.com>
2014-02-07 21:57:00 +0100

    .

--- js/troda.js
+++ js/troda.js
@@ -1,5 +1,24 @@
-        function troda_getURLParameter(name) {
-            return decodeURI(
-                    (RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[,null])[1]
-            );
-        }
+function getURLParameter(name) {
+    return decodeURI(
+        (RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[,null])[1]
+    );
+};
+function getTask(taskId){
+    return {
+        id:taskId,
+        name:"taskName",
+        location:"12,12"
+    };
+
+};
+
+var troda=function(){
+    return{
+        id:getURLParameter("trodaid"),
+        name:"someName",
+        tasks:[
+            getTask(1234)
+        ]
+    }
+
+}();
