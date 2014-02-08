879c3ed4ed325b93c0a6c1af54db9853a4bb3d60
Sjur Kvammen <sjur.kvammen@gmail.com>
2014-02-08 11:20:22 +0100

    integration with kulturminner

--- js/troda.js
+++ js/troda.js
@@ -27,20 +27,30 @@ function loadTroda(){
     var trodaId = getURLParameter("trodaId");
     $.get("http://localhost:3007/api/troda/"+trodaId,
         function(data){
-            $("#trodaName").text(data.name);
+            $("#nameOfTroda").text(data.name);
             console.log(data);
     });
     return trodaId;
 }
 
-function editTask(){
+function editTask(marker){
     editor=document.getElementById("edit_task");
     return editor;
 }
 
+
+
+function addPoint(e){
+    var marker = L.marker(e.latlng);
+    marker.addTo(map);
+    markers.push(marker);
+    marker.bindPopup(editTask(marker));
+    $.post("http://localhost:3007/api/troda/" + trodaId + "/tasks" ,{ lat: e.lat, lon: e.lon }, function(){console.log('added lonlat');});
+}
+
+
 function createMap(divName, trodaId){
     var map;
-    var markers = [];
     var topo = L.tileLayer('http://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=topo2&zoom={z}&x={x}&y={y}', {
         maxZoom: 16,
         attribution: '<a href="http://www.statkart.no/">Statens kartverk</a>'
@@ -52,10 +62,8 @@ function createMap(divName, trodaId){
 
     function addPoint(e){
         var marker = L.marker(e.latlng);
-
         marker.addTo(map);
-        markers.push(marker);
-        marker.bindPopup(editTask());
+        marker.bindPopup(editTask(marker));
         $.post("http://localhost:3007/api/troda/" + trodaId + "/tasks" ,{ lat: e.lat, lon: e.lon }, function(){console.log('added lonlat');});
     }
 
@@ -98,8 +106,25 @@ function createMap(divName, trodaId){
                 callback: zoomOut
             }]});
 
+
+    return map;
 }
 
 function changeLayer(cb) {
     console.log("layer"+cb.id+"set to " + cb.checked);
+}
+
+function loadPoints(map, dataset){
+    var bounds=map.getBounds();
+    var minll=bounds.getSouthWest();
+    var maxll=bounds.getNorthEast();
+
+    $.get("http://localhost:3007/api/finn/" + dataset +"/?bbox="+minll.lng+','+minll.lat+','+maxll.lng+','+maxll.lat, function (data){
+        console.log(data[0]);
+        for (var i=0; i < data.length; i++){
+            var marker = L.geoJson(data[i].geojson).addTo(map);
+            marker.bindPopup(editTask(marker));
+        }
+
+    });
 }
